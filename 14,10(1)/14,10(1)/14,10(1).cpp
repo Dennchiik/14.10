e76ec68a176fcd76f225c77f02cea9feb39eb985
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2023-10-16 19:20:20 +0300

    Add files via upload

---
+++ 14,10(1)/14,10(1)/14,10(1).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+
+std::string replaceDotsWithExclamation(const std::string& input) {
+    std::string modifiedString = input;
+
+    for (size_t i = 0; i < modifiedString.length(); ++i) {
+        if (modifiedString[i] == '.') {
+            modifiedString[i] = '!';
+        }
+    }
+
+    return modifiedString;
+}
+
+int main() {
+    std::string inputString = "Привет. Как дела?";
+    std::string result = replaceDotsWithExclamation(inputString);
+    std::cout << result << std::endl;
+
+    return 0;
+}
---
+++ 14,10(2)/14,10(2)/14,10(2).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+int countOccurrences(const std::string& inputString, char targetChar) {
+    int count = 0;
+    for (char c : inputString) {
+        if (c == targetChar) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    std::string strings[] = { "Hello, world!", "This is a test.", "The quick brown fox." };
+
+    char targetCharacter;
+    std::cout << "Введите символ для подсчета: ";
+    std::cin >> targetCharacter;
+
+    for (const std::string& str : strings) {
+        int count = countOccurrences(str, targetCharacter);
+        std::cout << "Символ '" << targetCharacter << "' встречается " << count << " раз в строке: " << str << std::endl;
+    }
+
+    return 0;
+}
